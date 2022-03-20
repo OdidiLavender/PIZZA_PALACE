45d22255072c830fa06de97fb5f02b457bb7a9e4
OdidiLavender <odidilavender@gmail.com>
2022-03-21 02:43:15 +0300

    added the user interface part

--- js/script.js
+++ js/script.js
@@ -33,4 +33,20 @@ function Pizza(size, quantity, crust, orderType){
 Pizza.prototype.total = function(price){
     return price * this.quantity;
 }
+// UI Logic
+$(document).ready(function(){
+
+    var pizzas = [
+        {name: "Pepperoni", id: "pepperoni", img: "./images/pepperoni.png", price: 950}, 
+        {name: "Hawaiian", id: "hawaiian", img: "./images/hawaiian.png", price: 550}, 
+        {name: "Margherita", id: "margherita", img: "./images/margherita.png", price: 450},
+        {name: "Chicken Tikka", id: "chicken", img: "./images/chicken.png", price: 850},
+        {name: "Sausage & Kale", id: "sausage", img: "./images/sausage.png", price: 650},
+        {name: "Bbq Beef", id: "bbq", img: "./images/bbq.png", price: 750}
+    ];
+
+    var pizzaPrice;
+    var pizzaName;
+
+
 
