1adc24c2f741d428c91621f5e45fa7b7b8bf9d3a
OdidiLavender <odidilavender@gmail.com>
2022-03-21 01:54:47 +0300

    used the constractor method on the contact scripting

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Business Logic
+// Name constructor
+function Contact(first, last, phone, email, address){
+    this.firstName = first;
+    this.lastName = last;
+    this.phoneNumber = phone;
+    this.emailAddress = email;
+    this.address = address;
+}
