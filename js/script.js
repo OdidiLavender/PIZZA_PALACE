e0fcb6011d52a3b2a65f365ffd5d4a71f4c37766
OdidiLavender <odidilavender@gmail.com>
2022-03-21 01:59:41 +0300

    used the constructor method in the the selection section

--- js/script.js
+++ js/script.js
@@ -7,3 +7,30 @@ function Contact(first, last, phone, email, address){
     this.emailAddress = email;
     this.address = address;
 }
+// Name prototype
+Contact.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName;
+}
+// // Addresses constructor
+// function Address(county, address){
+//     this.county = county;
+//     this.homeAddress = address;
+// }
+
+// // Addresses prototype
+// Address.prototype.fullAddress = function(){
+//     return this.homeAddress + ", " + this.county;
+// }
+
+// pizza constructor
+function Pizza(size, quantity, crust, orderType){
+    this.size = size;
+    this.quantity = quantity;
+    this.crust = crust;
+    this.orderType = orderType;
+}
+
+Pizza.prototype.total = function(price){
+    return price * this.quantity;
+}
+
